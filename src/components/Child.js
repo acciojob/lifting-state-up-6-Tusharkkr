3ef3c9ef009a988bed8af07643b9c31a46d9255f
Tushar kumar <narutoboi41@gmail.com>
2026-04-13 15:00:50 +0530

    check

--- src/components/Child.js
+++ src/components/Child.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 
 let Child = ({arr}) => {
    
-    
+
     return (
         <div>
             <h2>Child Component</h2>
             <ul>
              {arr.map(value =>(
-                <li> {value} <button onClick={(e) => e.target.remove()}> Complete </button></li>
+                <li> {value} <button onClick={(e) => e.target.innerText = 'Completed'}> Complete </button></li>
              ))}
              </ul>
         </div>
